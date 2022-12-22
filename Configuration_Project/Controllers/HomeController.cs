97a0043f12d35a453ded33fc5c31d2d59e8cc4a7
Alexei Chebanu <0alexchebanu@gmail.com>
2022-12-22 17:45:38 +0200

    Add project files.

---
+++ Configuration_Project/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Configuration_Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration= configuration;
+        }
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+            //GET: Loads configurations values into a new Options object
+            WeatherApiOptions options = _configuration.GetSection("weatherapi").
+                Get<WeatherApiOptions>();
+            //Bind: Loads configurations values into existing Options object
+            WeatherApiOptions options2 = new WeatherApiOptions();
+            _configuration.GetSection("weatherapi").Bind(options);
+            //Both configurations in that way are the same
+
+            ViewBag.ClientID = options.ClientID;
+            /*ViewBag.ClientIDb = options2.ClientID;*/
+
+            ViewBag.ClientSecret = options.ClientSecret;
+/*            ViewBag.ClientSecretb = options.ClientSecret;*/
+
+
+            return View();
+        }
+    }
+}
---
+++ Configuration_Project/WeatherApiOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace Configuration_Project
+{
+    public class WeatherApiOptions
+    {
+        public string? ClientID { get; set; }
+        public string? ClientSecret { get; set; }
+    }
+}
